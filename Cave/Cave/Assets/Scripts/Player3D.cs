c3d3b3c26edab19f4d39dea5511e64639178815f
MasafumiYamamoto <mythmaker3633@yahoo.co.jp>
2016-06-27 13:49:38 +0900

    cave
    洞窟生成追加

---
+++ Cave/Cave/Assets/Scripts/Player3D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player3D : MonoBehaviour {
+	
+	Rigidbody rigidbody3D;
+	Vector3 velocity;
+	
+	void Start () {
+		rigidbody3D = GetComponent<Rigidbody> ();
+	}
+	
+	void Update () {
+		velocity = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized * 10;
+	}
+	
+	void FixedUpdate() {
+		rigidbody3D.MovePosition (rigidbody3D.position + velocity * Time.fixedDeltaTime);
+	}
+}
